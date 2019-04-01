e3132bf5133aa96213f1e599de528f13e8b15525
EmirFeratovic <eferatovic1>
2019-04-01 10:41:45 +0200

    razvoj-softvera-t4

--- src/sample/Artikal.java
+++ src/sample/Artikal.java
@@ -43,6 +43,28 @@ public class Artikal {
                 '}';
     }
 
+    public String getSifra() {
+        return sifra;
+    }
 
+    public void setSifra(String sifra) {
+        this.sifra = sifra;
+    }
+
+    public String getNaziv() {
+        return naziv;
+    }
+
+    public void setNaziv(String naziv) {
+        this.naziv = naziv;
+    }
+
+    public double getCijena() {
+        return cijena;
+    }
+
+    public void setCijena(double cijena) {
+        this.cijena = cijena;
+    }
 
 }
